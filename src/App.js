0b3b7f1ad0cec169b8d8fef02d27cd322cf29a5a
RyanAsh93 <blueheadryan93@yahoo.com>
2020-07-01 12:47:48 -0600

    added chart

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import GetData from './GetData';
 
+
 function App() {
   return (
     <div className="App">
+      
       <GetData />
     </div>
   );
