cf10ead27086ee0187fab293066dc54ebb5090f6
RyanAsh93 <blueheadryan93@yahoo.com>
2020-07-02 12:40:44 -0600

    added nutrition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import GetData from './GetData';
+import WeatherDemo from './WeatherDemo';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <WeatherDemo />
       <GetData />
     </div>
   );
